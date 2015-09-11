7ab73bc19f1070718d501d6d0d27fb449936a2cf
Smiril <Smiril@users.noreply.github.com>
2015-09-12 00:13:58 +0200

    Update W32_x64_buildos.sh

--- src/W32_x64_buildos.sh
+++ src/W32_x64_buildos.sh
@@ -3,3 +3,4 @@
 sudo zypper install mingw32-cross-gcc mingw32-cross-gcc-bootstrap mingw32-runtime
 
 make W32smstools && make W32smstools-gtk && make W32getsms
+make x64smstools && make x64smstools-gtk && make x64getsms
