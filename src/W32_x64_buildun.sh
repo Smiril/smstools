44ba28d1ec9841671472a720517b147a56c2693d
Smiril <Smiril@users.noreply.github.com>
2015-09-12 10:07:07 +0200

    Update W32_x64_buildun.sh

--- src/W32_x64_buildun.sh
+++ src/W32_x64_buildun.sh
@@ -2,5 +2,9 @@
 
 sudo apt-get install mingw-w64 mingw-w64-tools mingw-w64-common mingw-w64-i686-dev mingw-w64-x86-64-dev g++-mingw-w64-i686
 
-make W32smstools && make W32smstools-gtk && make W32getsms
-make x64smstools && make x64smstools-gtk && make x64getsms
+make W32smstools 
+make W32smstools-gtk 
+make W32getsms
+make x64smstools 
+make x64smstools-gtk 
+make x64getsms
