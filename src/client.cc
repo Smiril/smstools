ff1cff84d0ad3595f47f42d6168cf510df6b3662
Smiril <Smiril@users.noreply.github.com>
2015-11-21 10:38:38 +0100

    Update client.cc

--- src/client.cc
+++ src/client.cc
@@ -1,5 +1,6 @@
 /*
     C ECHO client example using sockets
+    TODO write ssl socket code openssl based
 */
 #include <stdio.h> //printf
 #include <string.h>    //strlen
@@ -14,52 +15,95 @@
 #include <memory>
 #include <errno.h>
 #include <sstream>
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+
+#define CHK_NULL(x) if ((x)==NULL) exit (1)
+#define CHK_ERR(err,s) if ((err)==-1) { perror(s); exit(1); }
+#define CHK_SSL(err) if ((err)==-1) { ERR_print_errors_fp(stderr); exit(2); }
 
 using namespace std;
 
 int main(int argc , char *argv[])
 {
-    int sock;
-    struct sockaddr_in server;
+    int err;
+  int sd;
+  struct sockaddr_in sa;
+  SSL_CTX* ctx;
+  SSL*     ssl;
+  X509*    server_cert;
+  char*    str;
+  char     buf [4096];
+  SSL_METHOD *meth;
+
+  OpenSSL_add_ssl_algorithms();
+  meth = TLS_client_method();
+  SSL_load_error_strings();
+  ctx = SSL_CTX_new (meth);                        
+  CHK_NULL(ctx);
+
+  /* ----------------------------------------------- */
+  /* Create a socket and connect to server using normal socket calls. */
     char message[1000] , server_reply[2000];
     int read_size;
     std::string v2 = "VERSION";
     
     //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
+    sd = socket(AF_INET , SOCK_STREAM , 0);
+    if (sd == -1)
     {
         printf("Could not create socket");
     }
     puts("Socket created");
-     
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 1131 );
+    CHK_ERR(sd, "socket");
+ 
+    memset(&sa, 0, sizeof(sa)); 
+    sa.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons( 1131 );              
+  
  
     //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    if ( err = connect(sd , (struct sockaddr *)&sa , sizeof(sa)) < 0)
     {
         perror("connect failed. Error");
         return 1;
     }
-     
+    CHK_ERR(err, "connect"); 
     puts("Connected");
-
-    while(read_size = recv(sock , server_reply , 2000 , 0) > 0){
-	
+/* Now we have TCP conncetion. Start SSL negotiation. */
+  
+  ssl = SSL_new (ctx);                         CHK_NULL(ssl);    
+  SSL_set_fd (ssl, sd);
+  err = SSL_connect (ssl);                     CHK_SSL(err);
+    
+  /* Following two steps are optional and not required for
+     data exchange to be successful. */
+  
+    while(read_size = SSL_read(ssl, server_reply, sizeof(server_reply) - 1) > 0){
+	CHK_SSL(err);
 	printf("%s",server_reply);
 	// PAM AUTH USER DETAILS
-	send(sock , "user" , strlen("user") , 0);
-	send(sock , "password" , strlen("password") , 0);
+	SSL_write (ssl, "user", strlen("user")); 
+	CHK_SSL(err);
+	//send(sd , "user" , strlen("user") , 0);
+	SSL_write (ssl, "password", strlen("password")); 
+	CHK_SSL(err);
+	//send(sd , "password" , strlen("password") , 0);
 	//PAM AUTH END
-	send(sock , "43676xxxxxxx,43676xxxxxxx\ntest message\ny\ny\nn\n" , strlen("43676xxxxxxx,43676xxxxxxx\ntest message\ny\ny\nn\n") , 0);
+	SSL_write (ssl, "43676xxxxxxx,43676xxxxxxx\ntest message\ny\ny\nn\n", strlen("43676xxxxxxx,43676xxxxxxx\ntest message\ny\ny\nn\n")); 
+	CHK_SSL(err);
+	//send(sd , "43676xxxxxxx,43676xxxxxxx\ntest message\ny\ny\nn\n" , strlen("43676xxxxxxx,43676xxxxxxx\ntest message\ny\ny\nn\n") , 0);
 	printf("DATA SENT !\n");
 	
 	
 	if(read_size == 0)
 	{
-        puts("Client disconnected");
+        printf("Server disconnect's");
         fflush(stdout);
 	break;
 	}
@@ -67,10 +111,11 @@ int main(int argc , char *argv[])
 	{
         perror("recv failed");
 	}
-    close(sock);
     } //while recv end
     //recv(sock , server_reply , 6 , 0);
     //printf("%s",server_reply);
-    
+    close(sd);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
     return 0;
 }
